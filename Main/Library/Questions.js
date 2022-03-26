1066a9672845b0bd5af818a88c9d3b461ff24863
James Reilly <jreilly2112@gmail.com>
2022-03-26 01:09:34 -0400

    created schema and questions

---
+++ Main/Library/Questions.js
@@ -0,0 +1,103 @@
+const toDo = function ()
+{
+    return toDoQuestion = 
+        {
+            type: 'list',
+            message: "What would you like to do?",
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+            name: 'choice',
+            validate: function (answer)
+            {
+                if(answer)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        };
+};
+
+const addDepartment = function ()
+{
+    return employeeQuestion = 
+        {
+            type: 'input',
+            message: "What is the name of the department?",
+            name: 'departmentName',
+        }
+};
+
+const addRole = function ()
+{
+    return roleQuestions = 
+    [
+        {
+            type: 'input',
+            message: "What is the name of the role?",
+            name: 'roleName',
+        },
+        {
+            type: 'input',
+            message: "What is the role's salary?",
+            name: 'roleSalary',
+        },
+        {
+            type: 'input',
+            message: "What is the department for this role?",
+            name: 'roleDepartment',
+        },
+        
+    ];
+};
+
+const addEmployee = function ()
+{
+    return employeeQuestions = 
+    [
+        {
+            type: 'input',
+            message: "What is the employee's first name?",
+            name: 'employeeFirstName',
+        },
+        {
+            type: 'input',
+            message: "What is the employee's last name?",
+            name: 'employeeLastName',
+        },
+        {
+            type: 'input',
+            message: "What is the employee's role?",
+            name: 'employeeRole',
+        },
+        {
+            type: 'input',
+            message: "Who is the employee's manager?",
+            name: 'employeeManager',
+        },
+        
+    ];
+};
+
+const updateEmployee = function ()
+{
+    return employeeQuestions = 
+    [
+        {
+            type: 'list',
+            message: "Chose an Employee",
+            choices:[],
+            name: 'employeeToUpdate',
+        },
+        {
+            type: 'input',
+            message: "What is the employee's new role?",
+            name: 'employeeNewRole',
+        }
+        
+    ];
+};
+
+module.exports =  { toDo, addDepartment, addRole, addEmployee, updateEmployee };
